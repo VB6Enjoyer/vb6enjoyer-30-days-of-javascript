4a094cb99fb792ac0b9850c1171de405b9d4d90c
Juani <87107074+VB6Enjoyer@users.noreply.github.com>
2025-02-21 06:46:38 -0300

    Delete Day 1 directory

--- Day 1/comments.js
+++
@@ -1,8 +0,0 @@
-// Comments can make code readable // 1
-// Welcome to 30DaysOfJavaScript // 2
-
-/* // 3
-    Comments can make code readable,
-    easy to use and informative.
-*/
-
--- Day 1/datatypes.js
+++
@@ -1,5 +0,0 @@
-// 5
-console.log(typeof ('Za warudo!'));
-console.log(typeof (true));
-console.log(typeof (undefined));
-console.log(typeof (null));
